a1130c5d91db6b5f9dad508dd8002d98e24361e9
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 17:21:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
 function iterativeLog(array) {
-    array.forEach()
+  array.forEach((el, i) => {
+    console.log(`${i}: ${el}`)
+  })
 }
+
+function iterate(callback) {
+  const cats = ["Garfield", "Bianca", "Petunia"]
+
+  cats.forEach(callback)
+
+  return cats
+}
+
+function doToArray(array, callback) {
+ar
